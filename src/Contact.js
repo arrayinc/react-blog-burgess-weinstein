22bf53ee13a7445d8cfef7d434057bf9bb0fea40
Eli Burgess <eburgess.nexstep@gmail.com>
2021-03-26 10:24:00 -0600

    added a form to the contact page and added text and a button with an alert attached to it.

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import Letter from "./About/Letter.jpeg";
 
 function ContactPage() {
@@ -7,10 +7,32 @@ function ContactPage() {
     <Container className="ContainerMargin">
       <Row>
         <Col>
-          <img src={Letter} alt="Letter and quill" className="Letter"/>
+          <img src={Letter} alt="Letter and quill" className="Letter" />
         </Col>
         <Col>
-          Unfortunately, we are the ones who knock.
+          <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Reason Why</Form.Label>
+              <Form.Control as="select">
+                <option>N/A</option>
+                <option>You love the page</option>
+                <option>Your Matt and wish to tell us how awesome we are</option>
+                <option>Wish to go to the Continent</option>
+                <option>Want to give us money</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Brief decription of why your contacting us today</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Button variant="danger" type="submit" onClick={() => { alert('Thanks for submitting!'); }}>
+              Submit
+  </Button>
+          </Form>
         </Col>
       </Row>
     </Container>
