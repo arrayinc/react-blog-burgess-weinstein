f92b8045eaae45c5481bd5c71610b6d342c9c026
Eli Burgess <eburgess.nexstep@gmail.com>
2021-03-29 10:04:34 -0600

    added and styled a content container for the authors page

--- src/Authors.js
+++ src/Authors.js
@@ -84,11 +84,9 @@ function TheAuthors() {
           </Accordion>
         </Col>
       </Row>
-      <Row>
-        {/* <Col>Yennefer  <br /> Born into poverty yet now here she stands as advisor to kings and Witchers alike. A massive fan of the finer things in life, Yennefer graces readers with some of the best dishes she has tasted at kings banquets. Should you find yourself in a Kings Court surely you’ll become acquainted.  </Col> */}
-        {/* <Col>Geralt of Rivia <br /> Geralt is a Witcher from the School of the Wolf . Though his backstory may be filled with darkness and very little happiness he manages to find slivers of happiness on the Continent where he has met friends such as Jaskier and Yennefer. Geralt doesn’t talk much, but that hasn’t stopped him from giving his take on some of the culinary experiences he's enjoyed on his travels . If ever you find a monter near home, simply “Toss a coin to your Witcher”</Col> */}
-        {/* <Col>Jaskier  <br /> Although he is not one to talk about himself, you've most likely heard endless recounts of his adventures with Geralt of Rivia, he is a humble bard after all. Having performed in many courts and tasted delicacies all across the Continent, Jaskier (aka Dandelion) was overjoyed to help school readers on some of the best dining he has experienced.</Col> */}
-      </Row>
+      <Container className="bio-container">
+        <p> filler content born and raised</p>
+      </Container>
     </Container>
   );
 }
