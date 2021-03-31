4acab86a1b4193e05670f7b6668f3e8e8ac81552
Eli Burgess <eburgess.nexstep@gmail.com>
2021-03-31 14:12:01 -0600

    added background image and styled the form box

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,45 +1,42 @@
 import React from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
-import Letter from "../Images/Contact/Letter.jpeg";
 import ContactModal from "./Modal";
+
+
 function ContactPage() {
   return (
-    <Container className="ContainerMargin">
-      <Row>
-        <Col>
-          <img src={Letter} alt="Letter and quill" className="Letter" />
-        </Col>
-        <Col>
-          <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlSelect1">
-              <Form.Label>Reason Why</Form.Label>
-              <Form.Control as="select">
-                <option>N/A</option>
-                <option>You love the page</option>
-                <option>
-                  You are Matt and wish to tell us how awesome we are
-                </option>
-                <option>Wish to go to the Continent</option>
-                <option>Want to give us money</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>
-                Brief decription of why you're contacting us today
-              </Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-            <>
-              <ContactModal />
-            </>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <div className="transparent">
+
+      <Form className="form-container">
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Reason Why</Form.Label>
+          <Form.Control as="select">
+            <option>N/A</option>
+            <option>You love the page</option>
+            <option>
+              You are Matt and wish to tell us how awesome we are
+            </option>
+            <option>Wish to go to the Continent</option>
+            <option>Want to give us money</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>
+            Brief decription of why you're contacting us today
+          </Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <>
+          <ContactModal />
+        </>
+      </Form>
+    </div>
+
+
   );
 }
 export default ContactPage;
