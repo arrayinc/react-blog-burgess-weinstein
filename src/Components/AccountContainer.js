20dd35875d466168550ed5e0bb71cca530a09e8c
Eli Burgess <eburgess.nexstep@gmail.com>
2021-04-03 09:25:46 -0600

    added comments to AccountContainer, AccountContext, Authors, LoclaStorage, LoginForm,Signup, SignupForm

--- src/Components/AccountContainer.js
+++ src/Components/AccountContainer.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { LoginForm } from "./LoginForm";
 import { AccountContext } from "./AccountContext";
 import { SignUpForm } from "./SignupForm";
-
+//  home page for the sign up sheet
 export function AccountBox(props) {
+    //sets the active/default state of the page  
   const [active, setActive] = useState("signin");
 
   const switchToSignup = () => {
@@ -12,12 +13,13 @@ export function AccountBox(props) {
   const switchToSignin = () => {
     setActive("signin");
   };
-
+// wraps this conent of AccountContainer to pass props without fully passing props (kind of a cheat)
   const contextValue = { switchToSignin, switchToSignup };
-
+//whats appearing in the page
   return (
     <AccountContext.Provider value={contextValue}>
       <div className="sign-in-container sign-in-container-img greatprimer-font">
+          {/* psuedo links between login and signup to make the active render */}
         {active === "signin" && <LoginForm />}
         {active === "signup" && <SignUpForm />}
       </div>
--- src/Components/AccountContext.js
+++ src/Components/AccountContext.js
@@ -1,3 +1,3 @@
 import {createContext} from "react";
-
+//creates the link to switch between the login and signup by wraping the content in the AccountContainer
 export const AccountContext = createContext(); 
